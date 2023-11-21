07f840a179e28332d83978d9d303dcb516b401f5
Корнеева Марина <mkorneeva@sberhealth.ru>
2023-11-21 14:55:41 +0300

    2. Использование модуля assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,5 +1,16 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
+
+// assert(capitalize('') === '');
+// assert(capitalize('hello') === 'Hello');
+
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+assert.deepEqual({ key: 'value' }, { key: 'value' });
+
+
+/* 1
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
 }
@@ -7,5 +18,6 @@ if (capitalize('hello') !== 'Hello') {
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
 }
+*/
 
 console.log('Все тесты пройдены!');
